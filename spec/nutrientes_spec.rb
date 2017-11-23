dd87b86d20c48ff5d40afd754e5823a0ca667db7
Kristian Martinez <alu0100830200@ull.edu.es>
2017-11-23 22:21:46 +0100

    Añadidos los alimentos de la practica 5

--- spec/nutrientes_spec.rb
+++ spec/nutrientes_spec.rb
@@ -2,26 +2,36 @@ require "spec_helper.rb"
 
 describe Nutrientes::Nutrientes do
     before :each do
-        @ali0 = Nutrientes::Nutrientes.new("Huevo frito",14.1,0.0,19.5)
-        @ali1 = Nutrientes::Nutrientes.new("Leche vaca",3.3,4.8,3.2)
-        @ali2 = Nutrientes::Nutrientes.new("Yogurt",3.8,4.9,3.8)
-        @ali3 = Nutrientes::Nutrientes.new("Cerdo",21.5,0.0,6.3)
-        @ali4 = Nutrientes::Nutrientes.new("Ternera",21.1,0.0,3.1)
-        @ali5 = Nutrientes::Nutrientes.new("Pollo",20.6,0.0,5.6)
-        @ali6 = Nutrientes::Nutrientes.new("Bacalao",17.7,0.0,0.4)
-        @ali7 = Nutrientes::Nutrientes.new("Atún",21.5,0.0,15.5)
-        @ali8 = Nutrientes::Nutrientes.new("Salmón",19.9,0.0,13.6)
-        @ali9 = Nutrientes::Nutrientes.new("Aceite de oliva",0.0,0.2,99.6)
-        @ali10 = Nutrientes::Nutrientes.new("Chocolate",5.3,47.0,30.0)
-        @ali11 = Nutrientes::Nutrientes.new("Azúcar",0.0,99.8,0.0)
-        @ali12 = Nutrientes::Nutrientes.new("Arroz",6.8,77.7,0.6)
-        @ali13 = Nutrientes::Nutrientes.new("Lentejas",23.5,52.0,1.4)
-        @ali14 = Nutrientes::Nutrientes.new("Papas",2.0,15.4,0.1)
-        @ali15 = Nutrientes::Nutrientes.new("Tomate",1.0,3.5,0.2)
-        @ali16 = Nutrientes::Nutrientes.new("Cebolla",1.3,5.8,0.3)
-        @ali17 = Nutrientes::Nutrientes.new("Manzana",0.3,12.4,0.4)
-        @ali18 = Nutrientes::Nutrientes.new("Plátanos",1.2,21.4,0.2)
-        @ali19 = Nutrientes::Nutrientes.new("Calabaza",1.1,4.8,0.1)
+        @ali0 = Nutrientes::Nutrientes.new("Huevo frito",14.1,0.0,19.5,0)
+        @ali1 = Nutrientes::Nutrientes.new("Leche vaca",3.3,4.8,3.2,0)
+        @ali2 = Nutrientes::Nutrientes.new("Yogurt",3.8,4.9,3.8,0)
+        @ali3 = Nutrientes::Nutrientes.new("Cerdo",21.5,0.0,6.3,0)
+        @ali4 = Nutrientes::Nutrientes.new("Ternera",21.1,0.0,3.1,0)
+        @ali5 = Nutrientes::Nutrientes.new("Pollo",20.6,0.0,5.6,0)
+        @ali6 = Nutrientes::Nutrientes.new("Bacalao",17.7,0.0,0.4,0)
+        @ali7 = Nutrientes::Nutrientes.new("Atún",21.5,0.0,15.5,0)
+        @ali8 = Nutrientes::Nutrientes.new("Salmón",19.9,0.0,13.6,0)
+        @ali9 = Nutrientes::Nutrientes.new("Aceite de oliva",0.0,0.2,99.6,0)
+        @ali10 = Nutrientes::Nutrientes.new("Chocolate",5.3,47.0,30.0,0)
+        @ali11 = Nutrientes::Nutrientes.new("Azúcar",0.0,99.8,0.0,0)
+        @ali12 = Nutrientes::Nutrientes.new("Arroz",6.8,77.7,0.6,0)
+        @ali13 = Nutrientes::Nutrientes.new("Lentejas",23.5,52.0,1.4,0)
+        @ali14 = Nutrientes::Nutrientes.new("Papas",2.0,15.4,0.1,0)
+        @ali15 = Nutrientes::Nutrientes.new("Tomate",1.0,3.5,0.2,0)
+        @ali16 = Nutrientes::Nutrientes.new("Cebolla",1.3,5.8,0.3,0)
+        @ali17 = Nutrientes::Nutrientes.new("Manzana",0.3,12.4,0.4,0)
+        @ali18 = Nutrientes::Nutrientes.new("Plátanos",1.2,21.4,0.2,0)
+        @ali19 = Nutrientes::Nutrientes.new("Calabaza",1.1,4.8,0.1,0)
+
+        @chocolate = Nutrientes::Nutrientes.new("Chocolate",0,0,0,[[6.5,6.5,6.7,6.5,6.5,6.8,6.7,6.2,6.5,7.2,6.9,7.0,6.3,6.2,6.1,5.9,5.8,6.1,6.7,6.7,6.6,6.7,6.9,7.2,7.1],
+        [4.6,4.6,4.7,4.7,4.8,4.7,4.8,4.8,4.6,4.4,4.7,4.7,4.8,4.7,5.2,5.9,5.9,5.7,5.4,5.3,5.1,4.8,4.8,4.9,5.9]])
+        @yogurt = Nutrientes::Nutrientes.new("Yogurt",0,0,0,[[6.1,6.6,6.3,6.3,6.1,6.9,6.8,6.5,6.4,6.9,6.8,6.5,6.3,6.2,6.7,6.2,5.9,5.8,5.8,5.8,5.8,5.8,5.9,6.2,6.4],
+        [4.9,4.9,5.2,5.8,6.5,7.0,7.2,7.3,7.3,6.6,6.2,6.1,6.0,6.1,5.9,5.9,5.9,5.9,5.8,5.8,5.5,5.5,5.6,5.9,5.9]])
+        @glucosa = Nutrientes::Nutrientes.new("Glucosa",0,0,0,[[4.9,5.3,5.9,6.7,7.2,7.6,8.0,8.2,8.2,8.4,8.3,8.3,8.0,7.5,7.1,6.8,6.8,6.9,6.8,6.3,6.2,6.3,6.2,6.3,6.1],
+        [6.3,5.4,5.6,5.7,6.5,7.4,7.9,7.4,7.7,7.9,7.9,7.8,7.8,7.8,8.0,8.5,9.4,10.8,10.5,9.1,8.9,8.3,7.7,7.6,7.5],
+        [4.9,4.9,5.2,5.8,6.5,7.0,7.2,7.3,7.3,6.6,6.2,6.1,6.0,6.1,5.9,5.9,5.9,5.9,5.8,5.8,5.5,5.5,5.6,5.9,5.9]])
+        @compota = Nutrientes::Nutrientes.new("Compota",0,0,0,[[6.7,6.5,6.8,6.9,7.0,7.1,6.9,6.9,6.9,6.7,6.9,7.3,7.0,7.0,7.2,7.1,6.8,7.2,7.3,7.0,6.8,6.7,6.8,6.7,6.9],
+    [4.6,4.8,5.3,5.6,6.1,6.5,6.6,7.0,7.0,6.8,6.4,6.3,6.1,6.1,6.2,6.0,6.1,6.1,6.2,6.3,6.4,6.1,6.1,5.7,5.9]])
         
     end
 
